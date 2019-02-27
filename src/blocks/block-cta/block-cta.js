8d0ab897fa2019b62e5c1e878db819eb3e9d857b
Sam Gates <sam@covertnine.com>
2019-02-27 11:23:45 -0600

    everything working before i mess with stuff

--- src/blocks/block-cta/block-cta.js
+++ src/blocks/block-cta/block-cta.js
@@ -47,95 +47,9 @@ const {
 	Toolbar,
 } = wp.components;
 
-const blockAttributes = {
-	buttonText: {
-		type: 'string',
-	},
-	buttonUrl: {
-		type: 'string',
-		source: 'attribute',
-		selector: 'a',
-		attribute: 'href',
-	},
-	buttonAlignment: {
-		type: 'string',
-		default: 'center'
-	},
-	buttonBackgroundColor: {
-		type: 'string',
-		default: '#3373dc'
-	},
-	buttonTextColor: {
-		type: 'string',
-		default: '#ffffff'
-	},
-	buttonSize: {
-		type: 'string',
-		default: 'ab-button-size-medium'
-	},
-	buttonShape: {
-		type: 'string',
-		default: 'ab-button-shape-rounded'
-	},
-	buttonTarget: {
-		type: 'boolean',
-		default: false
-	},
-	ctaTitle: {
-		type: 'array',
-		selector: '.ab-cta-title',
-		source: 'children',
-	},
-	titleFontSize: {
-		type: 'number',
-		default: '32',
-	},
-	ctaTextFontSize: {
-		type: 'number',
-	},
-	ctaText: {
-		type: 'array',
-		selector: '.ab-cta-text',
-		source: 'children',
-	},
-	ctaWidth: {
-		type: 'string',
-	},
-	ctaBackgroundColor: {
-		type: 'string',
-	},
-	ctaTextColor: {
-		type: 'string',
-		default: '#32373c'
-	},
-	imgURL: {
-		type: 'string',
-		source: 'attribute',
-		attribute: 'src',
-		selector: 'img',
-	},
-	imgID: {
-		type: 'number',
-	},
-	imgAlt: {
-		type: 'string',
-		source: 'attribute',
-		attribute: 'alt',
-		selector: 'img',
-	},
-	dimRatio: {
-		type: 'number',
-		default: 50,
-	},
+import blockAttributes from './attributes' 
 
-	// Deprecated
-	ctaTitleFontSize: {
-		type: 'string',
-		default: '32'
-	},
-};
-
-class ABCTABlock extends Component {
+class C9CTABlock extends Component {
 
 	render() {
 
@@ -312,7 +226,7 @@ registerBlockType( 'covertnine-blocks/c9-cta', {
 	},
 
 	// Render the block components
-	edit: ABCTABlock,
+	edit: C9CTABlock,
 
 	// Save the attributes and markup
 	save: function( props ) {
