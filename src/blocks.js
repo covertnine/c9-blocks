eb8cfc65c963fde40d5bbd914e84b3e2e3f38730
samirillion <sam@protonmail.com>
2019-07-11 21:25:35 -0500

    messing more with ui and common.scss

--- src/blocks.js
+++ src/blocks.js
@@ -16,4 +16,17 @@ import "./blocks/block-sharing/block-sharing.js";
 import "./blocks/block-cta/block-cta.js";
 import "./blocks/block-custom-heading/block-custom-heading.js";
 import "./blocks/block-card/block-card.js";
-import './blocks/block-post-grid/index.js';
+import "./blocks/block-post-grid/index.js";
+
+function addBlockClassName(props, blockType) {
+	if (blockType.name === "core/paragraph" || blockType.name === "covertnine-blocks/c9-custom-heading") {
+        return Object.assign( props, { className: 'cortexnooutline' } );
+	}
+	return props;
+}
+
+wp.hooks.addFilter(
+	"blocks.getSaveContent.extraProps",
+	"cortex-blocks/add-block-class-name",
+	addBlockClassName
+);
