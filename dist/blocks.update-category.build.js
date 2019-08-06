7c46050bbeda948e5accfdba5b80d5265d6c384e
Ka Wong <ka@covertnine.com>
2019-08-06 12:28:52 -0500

    Add markdown example for template rendering to serve as reference when creating templates

--- dist/blocks.update-category.build.js
+++ dist/blocks.update-category.build.js
@@ -3380,8 +3380,8 @@ const { __ } = wp.i18n;
 const { Component, Fragment } = wp.element;
 const { Modal, TabPanel, Tooltip, Icon } = wp.components;
 const { compose } = wp.compose;
-const { withDispatch } = wp.data;
-const { createBlock } = wp.blocks;
+const { withDispatch, withSelect } = wp.data;
+const { createBlock, rawHandler } = wp.blocks;
 
 
 
@@ -3392,6 +3392,8 @@ class TemplatesModal extends Component {
 	}
 
 	render() {
+		const { resetBlocks, canUserUseUnfilteredHTML } = this.props;
+
 		const layouts = {
 			default: [createBlock("core/paragraph", {})],
 			hero: [createBlock("core/cover", { align: "full" }), createBlock("core/button", {
@@ -3404,10 +3406,25 @@ class TemplatesModal extends Component {
 			featured: [createBlock("core/heading", {}), createBlock("core/spacer", { height: "10" }), createBlock("core/media-text", { align: "full" }), createBlock("core/spacer", { height: "40" }), createBlock("core/quote", {}), createBlock("core/spacer", { height: "20" }), createBlock("core/media-text", { mediaPosition: "right" }), createBlock("core/paragraph", {
 				placeholder: __("Layout Switcher", "c9-blocks")
 			})],
-			nested: [createBlock("c9-blocks/grid", {}, [createBlock("c9-blocks/column-container", { columns: 3, layout: "c9-3-col-equal" }, [createBlock("c9-blocks/column", {}, [createBlock("core/button", { text: "Make this Recipe" })])])])]
+			nested: [createBlock("c9-blocks/grid", {}, [createBlock("c9-blocks/column-container", { columns: 3, layout: "c9-3-col-equal" }, [createBlock("c9-blocks/column", {}, [createBlock("core/button", { text: "Make this Recipe" })])])])],
+			markdown: rawHandler({
+				HTML: `<!-- wp:c9-blocks/horizontal-tabs {"tabActive":"tab-apple","buttonsAlign":"center","tabsData":[{"slug":"tab-apple","title":"apple"},{"slug":"tab-banana","title":"banana"}],"instanceId":0} -->
+				<div class="c9-horizontal-tabs" data-tab-active="tab-apple"><ul class="nav nav-tabs d-flex justify-content-center" role="tablist"><li class="nav-item"><a data-toggle="tab" role="tab" href="#tab-tab-apple-0" class="nav-link active" id="tab-button-tab-apple">apple</a></li><li class="nav-item"><a data-toggle="tab" role="tab" href="#tab-tab-banana-0" class="nav-link" id="tab-button-tab-banana">banana</a></li></ul><div class="c9-tabs-content tab-content"><!-- wp:c9-blocks/horizontal-tabs-tab {"slug":"tab-apple","tabActive":"tab-apple","id":0} -->
+				<div class="c9-horizontal-tabs-tab tab-pane fade show active" role="tabpanel" id="tab-tab-apple-0" aria-labelledby="tab-apple"><!-- wp:c9-blocks/heading {"heading":"test"} -->
+				<div class="section-heading c9-heading text-left"><h1 class="h">test</h1></div>
+				<!-- /wp:c9-blocks/heading --></div>
+				<!-- /wp:c9-blocks/horizontal-tabs-tab -->
+				
+				<!-- wp:c9-blocks/horizontal-tabs-tab {"slug":"tab-banana","tabActive":"tab-apple","id":0} -->
+				<div class="c9-horizontal-tabs-tab tab-pane fade" role="tabpanel" id="tab-tab-banana-0" aria-labelledby="tab-banana"><!-- wp:paragraph -->
+				<p>easdw</p>
+				<!-- /wp:paragraph --></div>
+				<!-- /wp:c9-blocks/horizontal-tabs-tab --></div></div>
+				<!-- /wp:c9-blocks/horizontal-tabs -->`,
+				mode: "BLOCKS",
+				canUserUseUnfilteredHTML
+			})
 		};
-
-		const { resetBlocks } = this.props;
 
 		return React.createElement(
 			Modal,
@@ -3490,6 +3507,11 @@ class TemplatesModal extends Component {
 										icon: "wordpress",
 										label: __("Nested", "c9-blocks"),
 										layout: layouts.nested
+									}),
+									React.createElement(__WEBPACK_IMPORTED_MODULE_0__layout_button_jsx__["a" /* default */], {
+										icon: "wordpress",
+										label: __("Markdown", "c9-blocks"),
+										layout: layouts.markdown
 									})
 								),
 								React.createElement(
@@ -3531,7 +3553,14 @@ class TemplatesModal extends Component {
 	}
 }
 
-const TemplatesModalWithSelect = compose([withDispatch(dispatch => {
+const TemplatesModalWithSelect = compose([withSelect((select, { clientId }) => {
+	const { getBlock, canUserUseUnfilteredHTML } = select("core/editor");
+	const block = getBlock(clientId);
+	return {
+		block,
+		canUserUseUnfilteredHTML: canUserUseUnfilteredHTML()
+	};
+}), withDispatch(dispatch => {
 	const { resetBlocks, insertBlocks } = dispatch("core/editor");
 	return {
 		resetBlocks,
