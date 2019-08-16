22651ae058549a93964e6990db256471fbe700e0
Ka Wong <ka@covertnine.com>
2019-08-16 17:26:02 -0500

    add back categories

--- src/blocks/block-column-container/block-column-container.js
+++ src/blocks/block-column-container/block-column-container.js
@@ -23,6 +23,8 @@ registerBlockType("c9-blocks/column-container", {
 	title: __("C9 Column Container", "c9-blocks"),
 	description: __("A responsive container for columns of content", "c9-blocks"),
 	icon: "columns",
+	category: "c9-blocks",
+	parent: ["c9-blocks/grid-container"],
 	supports: {
 		// fill in features
 		className: false
