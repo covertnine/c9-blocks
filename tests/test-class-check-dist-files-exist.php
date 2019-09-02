4219b9843916c73ad54411017e824fe3fbed8e1c
Ka Wong <ka@covertnine.com>
2019-09-02 15:55:08 -0500

    php lint fixing

--- tests/test-class-check-dist-files-exist.php
+++ tests/test-class-check-dist-files-exist.php
@@ -1,28 +1,29 @@
 <?php
-
 /**
  * Test `dist_file_exist`
  *
  * @package C9 Blocks
  */
 
-class Dist_File_Exist_Test extends WP_UnitTestCase
-{
-	/**
-	 * tests if dist contains all files
-	 */
-	function test_vendor_script_filename()
-	{
-		$cases = array('blocks.build.js', 'blocks.editor.build.css', 'blocks.frontend.build.js', 'blocks.style.build.css', 'blocks.update-category.build.js');
+/**
+ * Confirms if built files are in the dist folder
+ */
+class Dist_File_Exist_Test extends WP_UnitTestCase {
 
-		$path = dirname(__DIR__) . '/dist';
-		$files = scandir($path);
-		$files = array_diff(scandir($path), array('.', '..'));
+	/**
+	 * Tests if dist contains all files
+	 */
+	function test_vendor_script_filename() {
+		$cases = array( 'blocks.build.js', 'blocks.editor.build.css', 'blocks.frontend.build.js', 'blocks.style.build.css', 'blocks.update-category.build.js' );
+
+		$path  = dirname( __DIR__ ) . '/dist';
+		$files = scandir( $path );
+		$files = array_diff( scandir( $path ), array( '.', '..' ) );
 
 		$check = true;
 
-		foreach ($cases as $c) {
-			if (!in_array($c, $files)) {
+		foreach ( $cases as $c ) {
+			if ( ! in_array( $c, $files, true ) ) {
 				$check = false;
 			}
 		}
