2010c306c051419153697365b2ee1cfe99b10a3c
COVERT NINE <tim@covertnine.com>
2020-01-21 17:28:11 -0600

    updated conversion label for page templates

--- src/components/templates-modal/page-templates/page-types.js
+++ src/components/templates-modal/page-templates/page-types.js
@@ -1,5 +1,8 @@
 export default {
-    "awareness": "For Customers learning about your product, service, event for the first time. Give them the most information they need right upfront.",
-    "consideration": "Second touchpoint, Customers are familiar with you, but need some information.",
-    "conversion": "For Customers learning about your product, service, event for the first time. Give them the most information they need right upfront."
-}
+	awareness:
+		"For Customers learning about your product, service, event for the first time. Give them the most information they need right upfront.",
+	consideration:
+		"Second touchpoint, Customers are familiar with you, but need some information.",
+	conversion:
+		"For customers familiar with your product doing research before purchase or informed users ready to convert to customers."
+};
