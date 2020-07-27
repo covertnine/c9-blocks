c2e3ac9f7edb87064038c70777afa3c13921a0a8
Ka Wong <kawong2020@u.northwestern.edu>
2020-07-27 17:35:46 -0500

    add visual preview for c9 grid

--- src/blocks/block-grid-container/index.js
+++ src/blocks/block-grid-container/index.js
@@ -36,11 +36,124 @@ registerBlockType("c9-blocks/grid", {
 		"c9-blocks"
 	),
 
+	example: {
+		attributes: {
+			rows: 1
+		},
+		innerBlocks: [
+			{
+				name: "c9-blocks/column-container",
+				attributes: {
+					columns: 2,
+					layout: "c9-2-col-equal",
+					containerWidth: "container-narrow",
+					align: "narrow",
+					columnsGap: 10
+				},
+
+				innerBlocks: [
+					{
+						name: "c9-blocks/column",
+						attributes: {},
+						innerBlocks: [
+							{
+								name: "core/image",
+								attributes: {
+									sizeSlug: "large",
+									url:
+										"https://work.covertnine.com/wp-content/uploads/2020/07/set-of-tool-wrench-162553-55.jpg"
+								}
+							},
+							{
+								name: "c9-blocks/heading",
+								attributes: {
+									heading: "Use C9 Grids",
+									subheading: "for responsive content",
+									addSubheading: true,
+									tagLevel: 3
+								}
+							},
+							{
+								name: "core/paragraph",
+								attributes: {
+									content:
+										"To make your content responsive for all devices, put them inside of C9 Grids. That way, your users will see a scaled down version of your site that's laid out for a smaller screen automatically."
+								}
+							},
+							{
+								name: "core/buttons",
+								attributes: {},
+
+								innerBlocks: [
+									{
+										name: "core/button",
+										attributes: {}
+									},
+									{
+										name: "core/button",
+										attributes: {}
+									}
+								]
+							}
+						]
+					},
+					{
+						name: "c9-blocks/column",
+						attributes: {},
+						innerBlocks: [
+							{
+								name: "core/image",
+								attributes: {
+									sizeSlug: "large",
+									url:
+										"https://work.covertnine.com/wp-content/uploads/2020/07/clark-young-fQxMGkYXqFU-unsplash-55.jpg"
+								}
+							},
+							{
+								name: "c9-blocks/heading",
+								attributes: {
+									heading: "Column Responsiveness",
+									subheading: "Enable or Disable it",
+									addSubheading: true,
+									tagLevel: 3
+								}
+							},
+							{
+								name: "core/paragraph",
+								attributes: {
+									content:
+										"To make your content responsive for all devices, put them inside of C9 Grids. That way, your users will see a scaled down version of your site that's laid out for a smaller screen automatically."
+								}
+							},
+							{
+								name: "core/buttons",
+								attributes: {},
+
+								innerBlocks: [
+									{
+										name: "core/button",
+										attributes: {}
+									},
+									{
+										name: "core/button",
+										attributes: {}
+									}
+								]
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+
 	attributes,
 	// Render the block components
 	edit: compose([
 		withSelect((select, ownProps) => {
-			const { isBlockSelected, hasSelectedInnerBlock } = select("core/block-editor");
+			const { isBlockSelected, hasSelectedInnerBlock } = select(
+				"core/block-editor"
+			);
 
 			const { clientId } = ownProps;
 
