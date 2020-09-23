d890704c4f1ae2cd1a4ef30c7dac7ec6b4c77648
Ka Wong <kawong2020@u.northwestern.edu>
2020-09-22 20:17:42 -0500

    display keyframe buttons in column mode

--- src/components/animations-panel/keyframe-stepper.js
+++ src/components/animations-panel/keyframe-stepper.js
@@ -1,7 +1,20 @@
+import styled from "styled-components";
+
 const { __ } = wp.i18n;
 const { Button, ButtonGroup, BaseControl } = wp.components;
 
 const SIZE_PRESETS = [0, 25, 50, 75, 100];
+
+const StyledButtonGroup = styled(ButtonGroup)`
+	display: flex;
+	align-items: flex-start;
+	flex-direction: column;
+	flex-wrap: wrap;
+`;
+
+const StyledButton = styled(Button)`
+	min-width: 55px;
+`;
 
 const KeyframeStepper = props => {
 	const { label, currentValue, offset, resetValue, disabled } = props;
@@ -9,12 +22,12 @@ const KeyframeStepper = props => {
 	return (
 		<BaseControl label={label}>
 			<div className="block-editor-image-size-control__row">
-				<ButtonGroup aria-label={label}>
+				<StyledButtonGroup aria-label={label}>
 					{SIZE_PRESETS.map(scale => {
 						const buttonValue = `${offset}px ${scale}%`;
 						const isCurrent = buttonValue === currentValue;
 						return (
-							<Button
+							<StyledButton
 								disabled={disabled}
 								key={`${label}-${100 - scale}%`}
 								isPrimary={isCurrent}
@@ -22,10 +35,10 @@ const KeyframeStepper = props => {
 								onClick={() => props.handleClick(buttonValue)}
 							>
 								{100 - scale}%
-							</Button>
+							</StyledButton>
 						);
 					})}
-				</ButtonGroup>
+				</StyledButtonGroup>
 				<Button
 					disabled={disabled}
 					key={`${label}-reset`}
