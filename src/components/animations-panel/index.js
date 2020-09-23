f45d4b9c6a3f6375149831467c8ce7cc6b10c1cb
Ka Wong <kawong2020@u.northwestern.edu>
2020-09-22 21:01:22 -0500

    Animation panel is initially open + fix markers bug with selecting/deselecting block + refactor animation panel

--- src/components/animations-panel/index.js
+++ src/components/animations-panel/index.js
@@ -8,7 +8,7 @@ const {
 	BaseControl,
 	Disabled
 } = wp.components;
-const { Fragment, useEffect, useRef, useState } = wp.element;
+const { Fragment, useEffect, useRef } = wp.element;
 
 import SettingsSpacer from "../settings-spacer";
 import KeyframeStepper from "./keyframe-stepper";
@@ -26,10 +26,10 @@ const animationPanel = props => {
 		target,
 		animateStart,
 		animateEnd,
-		animateCustom
+		animateCustom,
+		useMarkers = false
 	} = props;
 
-	const [useMarkers, setUseMarkers] = useState(false);
 	const isFirstRun = useRef([true, true, true, true, true]);
 	const currDelay = useRef(animateDelay);
 	currDelay.current = animateDelay;
@@ -37,6 +37,10 @@ const animationPanel = props => {
 	currSpeed.current = animateSpeed;
 	const currCustom = useRef(animateCustom);
 	currCustom.current = animateCustom;
+
+	const setUseMarkers = value => {
+		setAttributes({ useMarkers: value });
+	};
 
 	// set new animation and kill old one
 	useEffect(() => {
@@ -160,8 +164,184 @@ const animationPanel = props => {
 		);
 	}, [animateCustom]);
 
+	const ScrollAnimatePanel = (
+		<Fragment>
+			<SelectControl
+				disabled={!animateScrub}
+				label={__("Animation", "c9-blocks")}
+				options={animateOptions}
+				value={animateVal}
+				onChange={animateVal => setAttributes({ animateVal })}
+			/>
+
+			<Button
+				disabled={!animateScrub}
+				onClick={() => {
+					const animateCustomBlank = {
+						before: {},
+						after: {}
+					};
+					setAttributes({
+						animateCustom: animateCustomBlank
+					});
+					currCustom.current = animateCustomBlank;
+				}}
+			>
+				Reset Settings
+			</Button>
+
+			{animateSettings[animateVal] && animateSettings[animateVal].before
+				? animateSettings[animateVal].before.map(c => {
+						let currValue = c.defaultValue;
+						if (
+							animateCustom.before &&
+							animateCustom.before[c.prop] !== undefined
+						) {
+							let storedValue = animateCustom.before[c.prop];
+							currValue =
+								"autoAlpha" === c.prop ? storedValue * 100 : storedValue;
+						}
+
+						return (
+							<RangeControl
+								disabled={!animateScrub}
+								label={__(c.name, "c9-blocks")}
+								value={currValue}
+								onChange={value => {
+									let savedValue = "autoAlpha" === c.prop ? value / 100 : value;
+
+									const newAnimateCustom = {
+										...animateCustom,
+										before: {
+											...animateCustom.before,
+											[c.prop]: savedValue
+										}
+									};
+									setAttributes({ animateCustom: newAnimateCustom });
+								}}
+								min={c.minValue}
+								max={c.maxValue}
+							/>
+						);
+				  })
+				: null}
+
+			{animateSettings[animateVal] && animateSettings[animateVal].after
+				? animateSettings[animateVal].after.map(c => {
+						let currValue = c.defaultValue;
+						if (
+							animateCustom.after &&
+							animateCustom.after[c.prop] !== undefined
+						) {
+							let storedValue = animateCustom.after[c.prop];
+							currValue =
+								"autoAlpha" === c.prop ? storedValue * 100 : storedValue;
+						}
+
+						return (
+							<RangeControl
+								disabled={!animateScrub}
+								label={__(c.name, "c9-blocks")}
+								value={currValue}
+								onChange={value => {
+									let savedValue = "autoAlpha" === c.prop ? value / 100 : value;
+
+									const newAnimateCustom = {
+										...animateCustom,
+										after: {
+											...animateCustom.after,
+											[c.prop]: savedValue
+										}
+									};
+									setAttributes({ animateCustom: newAnimateCustom });
+								}}
+								min={c.minValue}
+								max={c.maxValue}
+							/>
+						);
+				  })
+				: null}
+
+			<SettingsSpacer />
+
+			{animateScrub ? (
+				<ToggleControl
+					disabled={!animateScrub}
+					label={__("Show Keyframe Markers", "c9-blocks")}
+					checked={useMarkers}
+					onChange={useMarkers => setUseMarkers(useMarkers)}
+				/>
+			) : (
+				<Disabled>
+					<ToggleControl
+						disabled={!animateScrub}
+						label={__("Show Keyframe Markers", "c9-blocks")}
+						checked={useMarkers}
+						onChange={useMarkers => setUseMarkers(useMarkers)}
+					/>
+				</Disabled>
+			)}
+
+			<BaseControl
+				help={__(
+					"Select beginning and ending trigger positions in the browser window to add a scrub animation effect as the user scrolls between those positions.",
+					"c9-blocks"
+				)}
+			/>
+			<KeyframeStepper
+				disabled={!animateScrub}
+				label={__("Starting Keyframe", "c9-blocks")}
+				currentValue={animateStart}
+				handleClick={animateStart => setAttributes({ animateStart })}
+				offset={0}
+			/>
+			<SettingsSpacer />
+			<KeyframeStepper
+				disabled={!animateScrub}
+				label={__("Ending Keyframe", "c9-blocks")}
+				currentValue={animateEnd}
+				handleClick={animateEnd => setAttributes({ animateEnd })}
+				offset={
+					document.querySelector(target)
+						? document.querySelector(target).offsetHeight
+						: 0
+				}
+			/>
+		</Fragment>
+	);
+
+	const TransitionAnimatePanel = (
+		<Fragment>
+			<SelectControl
+				label={__("Animation", "c9-blocks")}
+				options={animateOptions}
+				value={animateVal}
+				onChange={animateVal => setAttributes({ animateVal })}
+			/>
+
+			<RangeControl
+				label={__("Delay (ms)", "c9-blocks")}
+				value={animateDelay}
+				onChange={animateDelay => {
+					setAttributes({ animateDelay });
+				}}
+				min={0}
+				max={5000}
+			/>
+			<RangeControl
+				label={__("Speed (ms)", "c9-blocks")}
+				value={animateSpeed}
+				onChange={animateSpeed => {
+					setAttributes({ animateSpeed });
+				}}
+				min={500}
+				max={5000}
+			/>
+		</Fragment>
+	);
+
 	return (
-		<PanelBody title={__("Animations", "c9-blocks")} initialOpen={false}>
+		<PanelBody title={__("Animations", "c9-blocks")}>
 			<ToggleControl
 				label={__("Enable/Disable Animations", "c9-blocks")}
 				checked={enableAnimate}
@@ -171,202 +351,32 @@ const animationPanel = props => {
 				<Fragment>
 					<SettingsSpacer />
 					<p className="components-base-control__label">
-						{__("Scroll Animation", "c9-blocks")}
-					</p>
-					<SelectControl
-						disabled={!animateScrub}
-						label={__("Animation", "c9-blocks")}
-						options={animateOptions}
-						value={animateVal}
-						onChange={animateVal => setAttributes({ animateVal })}
-					/>
-
-					<Button
-						disabled={!animateScrub}
-						onClick={() => {
-							const animateCustomBlank = {
-								before: {},
-								after: {}
-							};
-							setAttributes({
-								animateCustom: animateCustomBlank
-							});
-							currCustom.current = animateCustomBlank;
-						}}
-					>
-						Reset Settings
-					</Button>
-
-					{animateSettings[animateVal] && animateSettings[animateVal].before
-						? animateSettings[animateVal].before.map(c => {
-								let currValue = c.defaultValue;
-								if (
-									animateCustom.before &&
-									animateCustom.before[c.prop] !== undefined
-								) {
-									let storedValue = animateCustom.before[c.prop];
-									currValue =
-										"autoAlpha" === c.prop ? storedValue * 100 : storedValue;
-								}
-
-								return (
-									<RangeControl
-										disabled={!animateScrub}
-										label={__(c.name, "c9-blocks")}
-										value={currValue}
-										onChange={value => {
-											let savedValue =
-												"autoAlpha" === c.prop ? value / 100 : value;
-
-											const newAnimateCustom = {
-												...animateCustom,
-												before: {
-													...animateCustom.before,
-													[c.prop]: savedValue
-												}
-											};
-											setAttributes({ animateCustom: newAnimateCustom });
-										}}
-										min={c.minValue}
-										max={c.maxValue}
-									/>
-								);
-						  })
-						: null}
-
-					{animateSettings[animateVal] && animateSettings[animateVal].after
-						? animateSettings[animateVal].after.map(c => {
-								let currValue = c.defaultValue;
-								if (
-									animateCustom.after &&
-									animateCustom.after[c.prop] !== undefined
-								) {
-									let storedValue = animateCustom.after[c.prop];
-									currValue =
-										"autoAlpha" === c.prop ? storedValue * 100 : storedValue;
-								}
-
-								return (
-									<RangeControl
-										disabled={!animateScrub}
-										label={__(c.name, "c9-blocks")}
-										value={currValue}
-										onChange={value => {
-											let savedValue =
-												"autoAlpha" === c.prop ? value / 100 : value;
-
-											const newAnimateCustom = {
-												...animateCustom,
-												after: {
-													...animateCustom.after,
-													[c.prop]: savedValue
-												}
-											};
-											setAttributes({ animateCustom: newAnimateCustom });
-										}}
-										min={c.minValue}
-										max={c.maxValue}
-									/>
-								);
-						  })
-						: null}
-
-					<SettingsSpacer />
-
-					{animateScrub ? (
-						<ToggleControl
-							disabled={!animateScrub}
-							label={__("Show Keyframe Markers", "c9-blocks")}
-							checked={useMarkers}
-							onChange={useMarkers => setUseMarkers(useMarkers)}
-						/>
-					) : (
-						<Disabled>
-							<ToggleControl
-								disabled={!animateScrub}
-								label={__("Show Keyframe Markers", "c9-blocks")}
-								checked={useMarkers}
-								onChange={useMarkers => setUseMarkers(useMarkers)}
-							/>
-						</Disabled>
-					)}
-
-					<BaseControl
-						help={__(
-							"Select beginning and ending trigger positions in the browser window to add a scrub animation effect as the user scrolls between those positions.",
+						{__(
+							animateScrub ? "Scroll Animation" : "Transition Animation",
 							"c9-blocks"
 						)}
-					/>
-					<KeyframeStepper
-						disabled={!animateScrub}
-						label={__("Starting Keyframe", "c9-blocks")}
-						currentValue={animateStart}
-						handleClick={animateStart => setAttributes({ animateStart })}
-						offset={0}
-					/>
-					<SettingsSpacer />
-					<KeyframeStepper
-						disabled={!animateScrub}
-						label={__("Ending Keyframe", "c9-blocks")}
-						currentValue={animateEnd}
-						handleClick={animateEnd => setAttributes({ animateEnd })}
-						offset={
-							document.querySelector(target)
-								? document.querySelector(target).offsetHeight
-								: 0
+					</p>
+
+					<ToggleControl
+						label={__("Scroll/Transition", "c9-blocks")}
+						checked={!animateScrub}
+						onChange={animateScrub =>
+							setAttributes({ animateScrub: !animateScrub })
 						}
 					/>
-				</Fragment>
-			) : null}
 
-			<SettingsSpacer />
-			<p className="components-base-control__label">
-				{__("Transition Animation", "c9-blocks")}
-			</p>
+					{!animateScrub && (
+						<BaseControl
+							help={__(
+								"Settings to automatically animate the selected block with delay and speed options for fine tuning animtions.",
+								"c9-blocks"
+							)}
+						/>
+					)}
 
-			<SettingsSpacer />
-			<ToggleControl
-				label={__("Enable Transitions", "c9-blocks")}
-				checked={!animateScrub}
-				onChange={animateScrub =>
-					setAttributes({ animateScrub: !animateScrub })
-				}
-			/>
+					<SettingsSpacer />
 
-			{enableAnimate && !animateScrub ? (
-				<Fragment>
-					<BaseControl
-						help={__(
-							"Settings to automatically animate the selected block with delay and speed options for fine tuning animtions.",
-							"c9-blocks"
-						)}
-					/>
-
-					<SelectControl
-						label={__("Animation", "c9-blocks")}
-						options={animateOptions}
-						value={animateVal}
-						onChange={animateVal => setAttributes({ animateVal })}
-					/>
-
-					<RangeControl
-						label={__("Delay (ms)", "c9-blocks")}
-						value={animateDelay}
-						onChange={animateDelay => {
-							setAttributes({ animateDelay });
-						}}
-						min={0}
-						max={5000}
-					/>
-					<RangeControl
-						label={__("Speed (ms)", "c9-blocks")}
-						value={animateSpeed}
-						onChange={animateSpeed => {
-							setAttributes({ animateSpeed });
-						}}
-						min={500}
-						max={5000}
-					/>
+					{animateScrub ? ScrollAnimatePanel : TransitionAnimatePanel}
 				</Fragment>
 			) : null}
 		</PanelBody>
