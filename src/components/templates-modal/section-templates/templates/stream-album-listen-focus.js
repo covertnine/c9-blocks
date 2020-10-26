52e4fdf7e2837fcb4cda0a893f13ea00e1aad76a
tim <tim@covertnine.com>
2020-10-26 18:18:45 -0500

    reusable block preview layout changes

--- src/components/templates-modal/section-templates/templates/stream-album-listen-focus.js
+++ src/components/templates-modal/section-templates/templates/stream-album-listen-focus.js
@@ -4,59 +4,59 @@ export default {
 	icon: Icon,
 	title: "Stream Album Listen Focus",
 	description: "150 Words • Spotify Link • List • 3 Images",
-	markup: `<!-- wp:c9-blocks/grid {"instanceId":2,"rows":2,"containerImgURL":"https://c9.covertnine.com/wp-content/uploads/2020/10/30055127781_732e6b081b_o.jpg","containerHue":"#030303","overlayHue":"#212121","overlayOpacity":8,"minScreenHeight":10,"containerMargin":{"linked":true,"unit":"px","top":"5","bottom":"5","left":"5","right":"5","icon":"admin-links"},"containerPadding":{"linked":true,"top":"-1","bottom":"-1","left":"-1","right":"-1","icon":"admin-links"},"focalPoint":{"x":"0.51","y":"0.00"}} -->
-<div class="c9-grid my-5 c9-scroll c9-grid-has-background" style="min-height:10vh;background-color:rgba(3,3,3,1)"><!-- wp:c9-blocks/column-container {"columns":2,"layout":"c9-2-col-wideleft","minScreenHeight":42} -->
-<div class="container c9-column-container c9-scroll c9-layout-columns-2 c9-2-col-wideleft" style="min-height:42vh"><div class="c9-layout-column-wrap c9-block-layout-column-gap-2 c9-is-responsive-column"><!-- wp:c9-blocks/column {"verticalAlign":"bottom"} -->
-<div class="c9-block-layout-column c9-column text-left c9-is-vertically-aligned-bottom"><div class="c9-column-innner"><!-- wp:c9-blocks/heading {"heading":"Embed Spotify playlists with a simple link!","tagLevel":2,"displayLevel":"5","type":"c9-sh subhead-h","textColor":"#6d6d6d","overrideStyle":true} -->
-<div class="section-heading c9-heading text-left"><h2 class="c9-sh subhead-h5" style="color:#6d6d6d">Embed Spotify playlists with a simple link!</h2></div>
+	markup: `<!-- wp:c9-blocks/grid {"instanceId":3,"rows":2,"containerImgURL":"https://c9.covertnine.com/wp-content/uploads/2020/10/homo-riot-vertical.jpg","overlayHue":"#181818","overlayOpacity":10,"blendMode":"multiply","minScreenHeight":78,"focalPoint":{"x":"0.56","y":"0.89"}} -->
+<div class="c9-grid p-5 c9-scroll c9-grid-has-background" style="min-height:78vh"><!-- wp:c9-blocks/column-container {"align":"wide","columns":2,"columnsGap":10,"layout":"c9-2-col-wideright","verticalAlign":"center","minScreenHeight":80,"containerMargin":{"linked":true,"top":"5","bottom":"5","left":"5","right":"5","icon":"admin-links"},"containerPadding":{"linked":true,"top":"5","bottom":"5","left":"5","right":"5","icon":"admin-links"}} -->
+<div class="container alignwide c9-column-container p-5 my-5 c9-scroll c9-layout-columns-2 c9-is-vertically-aligned-center c9-2-col-wideright" style="min-height:80vh"><div class="c9-layout-column-wrap c9-block-layout-column-gap-10 c9-is-responsive-column"><!-- wp:c9-blocks/column {"verticalAlign":"center"} -->
+<div class="c9-block-layout-column c9-column text-left c9-is-vertically-aligned-center"><div class="c9-column-innner"><!-- wp:c9-blocks/heading {"heading":"Listen","subheading":"Spotify Album Listen","addSubheading":true,"displayLevel":"3","textColor":"#ffffff","weight":"bold","overrideStyle":true} -->
+<div class="section-heading c9-heading text-left"><h1 class="c9-h h3 font-weight-bold" style="color:#ffffff">Listen</h1><div class="c9-h h3"><small class="text-muted">&nbsp;Spotify Album Listen</small></div></div>
 <!-- /wp:c9-blocks/heading -->
 
-<!-- wp:c9-blocks/heading {"heading":"Stream Albums","tagLevel":2,"displayLevel":"1","textColor":"#ffffff","overrideStyle":true} -->
-<div class="section-heading c9-heading text-left"><h2 class="c9-h h1" style="color:#ffffff">Stream Albums</h2></div>
-<!-- /wp:c9-blocks/heading --></div></div>
+<!-- wp:list {"ordered":true,"className":"is-style-light-text"} -->
+<ol class="is-style-light-text"><li><strong>45</strong></li><li><strong>Handwritten</strong></li><li><strong>Here Comes My Man</strong></li><li><strong>Mulholland Drive</strong></li><li><strong>Keepsake</strong></li><li><strong>Too Much Blood</strong></li><li><strong>Howl</strong></li><li><strong>Biloxi Parish</strong></li><li><strong>Desire</strong></li><li><strong>Mae</strong></li><li><strong>National Anthem</strong></li><li><strong>Blue Dahlia</strong></li><li><strong>Silver</strong></li><li><strong>You Got Lucky</strong></li></ol>
+<!-- /wp:list --></div></div>
 <!-- /wp:c9-blocks/column -->
 
-<!-- wp:c9-blocks/column -->
-<div class="c9-block-layout-column c9-column text-left"><div class="c9-column-innner"></div></div>
+<!-- wp:c9-blocks/column {"verticalAlign":"center"} -->
+<div class="c9-block-layout-column c9-column text-left c9-is-vertically-aligned-center"><div class="c9-column-innner"><!-- wp:core-embed/spotify {"url":"https://open.spotify.com/album/1ti02OHxoRIdf8DAZqC2mu?si=r4iEXg8JS22kJewszyebxQ","type":"rich","providerNameSlug":"spotify","className":"wp-embed-aspect-9-16 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed-spotify wp-block-embed is-type-rich is-provider-spotify wp-embed-aspect-9-16 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://open.spotify.com/album/1ti02OHxoRIdf8DAZqC2mu?si=r4iEXg8JS22kJewszyebxQ
+</div></figure>
+<!-- /wp:core-embed/spotify --></div></div>
 <!-- /wp:c9-blocks/column --></div></div>
 <!-- /wp:c9-blocks/column-container -->
 
-<!-- wp:c9-blocks/column-container {"columns":3,"columnsGap":5,"layout":"c9-3-col-equal","overlayHue":"#0b0b0b","overlayOpacity":9,"minScreenHeight":0,"containerMargin":{"linked":false,"top":"5","bottom":"5","left":"-1","right":"-1","icon":"editor-unlink"},"containerPadding":{"linked":true,"top":"5","bottom":"5","left":"5","right":"5","icon":"admin-links"}} -->
-<div class="container c9-column-container p-5 my-5 c9-scroll c9-layout-columns-3 c9-3-col-equal"><div class="c9-overlay-container" style="background-color:rgba(11,11,11,.9);mix-blend-mode:overlay"></div><div class="c9-layout-column-wrap c9-block-layout-column-gap-5 c9-is-responsive-column"><!-- wp:c9-blocks/column {"verticalAlign":"top"} -->
-<div class="c9-block-layout-column c9-column text-left c9-is-vertically-aligned-top"><div class="c9-column-innner"><!-- wp:image {"id":3157,"sizeSlug":"large","className":"img-fluid"} -->
-<figure class="wp-block-image size-large img-fluid"><img src="https://c9.covertnine.com/wp-content/uploads/2020/10/c9-album-a-1024x1024.jpg" alt="" class="wp-image-3157"/></figure>
-<!-- /wp:image -->
+<!-- wp:c9-blocks/column-container {"columns":2,"columnsGap":10,"layout":"c9-2-col-equal","containerMargin":{"linked":true,"top":"5","bottom":"5","left":"5","right":"5","icon":"admin-links"},"containerPadding":{"linked":true,"top":"5","bottom":"5","left":"5","right":"5","icon":"admin-links"}} -->
+<div class="container c9-column-container p-5 my-5 c9-scroll c9-layout-columns-2 c9-2-col-equal" style="min-height:10vh"><div class="c9-layout-column-wrap c9-block-layout-column-gap-10 c9-is-responsive-column"><!-- wp:c9-blocks/column -->
+<div class="c9-block-layout-column c9-column text-left"><div class="c9-column-innner"><!-- wp:paragraph {"dropCap":true,"style":{"color":{"text":"#efefef"}}} -->
+<p class="has-drop-cap has-text-color" style="color:#efefef">Cursus euismod quis viverra nibh. Nisi est sit amet facilisis magna etiam. Fringilla ut morbi tincidunt augue interdum. Aliquam ut porttitor leo a diam sollicitudin tempor id eu. Semper eget duis at tellus at urna condimentum. Integer eget aliquet nibh praesent tristique magna sit amet purus. </p>
+<!-- /wp:paragraph -->
 
-<!-- wp:core-embed/spotify {"url":"https://open.spotify.com/album/49Q3EfStlDspsaOo8VAgtW?si=AFBKe_uuSPK80YWuxOODGA","type":"rich","providerNameSlug":"spotify","allowResponsive":false,"className":""} -->
-<figure class="wp-block-embed-spotify wp-block-embed is-type-rich is-provider-spotify"><div class="wp-block-embed__wrapper">
-https://open.spotify.com/album/49Q3EfStlDspsaOo8VAgtW?si=AFBKe_uuSPK80YWuxOODGA
-</div></figure>
-<!-- /wp:core-embed/spotify --></div></div>
+<!-- wp:paragraph {"style":{"color":{"text":"#efefef"}}} -->
+<p class="has-text-color" style="color:#efefef">Nisi vitae suscipit tellus mauris a. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"color":{"text":"#eeeeee"}}} -->
+<p class="has-text-color" style="color:#eeeeee">Et odio pellentesque diam volutpat commodo. Vitae sapien pellentesque habitant morbi tristique senectus et netus. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisqu.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"color":{"text":"#f8f8f8"}}} -->
+<p class="has-text-color" style="color:#f8f8f8">Ullamcorper malesuada proin libero nunc consequat interdum varius sit. Ultrices eros in cursus turpis.&nbsp;</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":33} -->
+<div style="height:33px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"align":"center","id":3192,"width":51,"height":38,"sizeSlug":"large"} -->
+<div class="wp-block-image"><figure class="aligncenter size-large is-resized"><img src="https://c9.covertnine.com/wp-content/uploads/2020/10/feather-logo-white.svg" alt="" class="wp-image-3192" width="51" height="38"/></figure></div>
+<!-- /wp:image --></div></div>
 <!-- /wp:c9-blocks/column -->
 
-<!-- wp:c9-blocks/column {"verticalAlign":"top"} -->
-<div class="c9-block-layout-column c9-column text-left c9-is-vertically-aligned-top"><div class="c9-column-innner"><!-- wp:image {"id":3158,"sizeSlug":"large","className":"img-fluid"} -->
-<figure class="wp-block-image size-large img-fluid"><img src="https://c9.covertnine.com/wp-content/uploads/2020/10/c9-album-b-1024x1024.jpg" alt="" class="wp-image-3158"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:core-embed/spotify {"url":"https://open.spotify.com/album/1nzDVfQopF9YBkpPAfUck3?si=lCYs7A3mRhuW34qG0YB_YA","type":"rich","providerNameSlug":"spotify","allowResponsive":false,"className":""} -->
-<figure class="wp-block-embed-spotify wp-block-embed is-type-rich is-provider-spotify"><div class="wp-block-embed__wrapper">
-https://open.spotify.com/album/1nzDVfQopF9YBkpPAfUck3?si=lCYs7A3mRhuW34qG0YB_YA
-</div></figure>
-<!-- /wp:core-embed/spotify --></div></div>
-<!-- /wp:c9-blocks/column -->
-
-<!-- wp:c9-blocks/column {"verticalAlign":"top"} -->
-<div class="c9-block-layout-column c9-column text-left c9-is-vertically-aligned-top"><div class="c9-column-innner"><!-- wp:image {"id":3159,"sizeSlug":"large","className":"img-fluid"} -->
-<figure class="wp-block-image size-large img-fluid"><img src="https://c9.covertnine.com/wp-content/uploads/2020/10/c9-album-c-1024x1024.jpg" alt="" class="wp-image-3159"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:core-embed/spotify {"url":"https://open.spotify.com/album/5baJxk2fGaLorySaf6173S?si=8-F5eOhyQ1ut_CkWnAUonQ","type":"rich","providerNameSlug":"spotify","allowResponsive":false,"className":""} -->
-<figure class="wp-block-embed-spotify wp-block-embed is-type-rich is-provider-spotify"><div class="wp-block-embed__wrapper">
-https://open.spotify.com/album/5baJxk2fGaLorySaf6173S?si=8-F5eOhyQ1ut_CkWnAUonQ
-</div></figure>
-<!-- /wp:core-embed/spotify --></div></div>
+<!-- wp:c9-blocks/column -->
+<div class="c9-block-layout-column c9-column text-left"><div class="c9-column-innner"><!-- wp:c9-blocks/image-carousel {"instanceId":0,"url":["https://c9.covertnine.com/wp-content/uploads/2020/10/c9-album-b-1024x1024.jpg","https://c9.covertnine.com/wp-content/uploads/2020/10/c9-album-a-1024x1024.jpg","https://c9.covertnine.com/wp-content/uploads/2020/10/c9-album-c-1024x1024.jpg"],"id":[3158,3157,3159],"captionTitle":[null,null,null],"captionContent":[null,null,null],"slideMaxHeight":455} -->
+<div id="c9-image-carousel-indicator-0" class="c9-image-carousel carousel slide container" data-ride="carousel" data-interval="5000" data-wrap="true"><ol class="carousel-indicators"><li data-target="#c9-image-carousel-indicator-0" data-slide-to="0" class="active"></li><li data-target="#c9-image-carousel-indicator-0" data-slide-to="1"></li><li data-target="#c9-image-carousel-indicator-0" data-slide-to="2"></li></ol><div class="carousel-inner"><div class="carousel-item active"><img src="https://c9.covertnine.com/wp-content/uploads/2020/10/c9-album-b-1024x1024.jpg" class="d-block w-100"/></div><div class="carousel-item"><img src="https://c9.covertnine.com/wp-content/uploads/2020/10/c9-album-a-1024x1024.jpg" class="d-block w-100"/></div><div class="carousel-item"><img src="https://c9.covertnine.com/wp-content/uploads/2020/10/c9-album-c-1024x1024.jpg" class="d-block w-100"/></div></div><a class="carousel-control-prev" href="#c9-image-carousel-indicator-0" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#c9-image-carousel-indicator-0" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a></div>
+<!-- /wp:c9-blocks/image-carousel --></div></div>
 <!-- /wp:c9-blocks/column --></div></div>
-<!-- /wp:c9-blocks/column-container --><div class="c9-overlay-container" style="background-color:rgba(33,33,33,.8);mix-blend-mode:overlay"></div><div class="c9-image-container c9-image-mobile-size-cover" style="background-position:51% 0%;background-image:url(https://c9.covertnine.com/wp-content/uploads/2020/10/30055127781_732e6b081b_o.jpg);background-repeat:no-repeat;background-size:cover"></div></div>
+<!-- /wp:c9-blocks/column-container --><div class="c9-overlay-container" style="background-color:rgba(24,24,24,1);mix-blend-mode:multiply"></div><div class="c9-image-container c9-image-mobile-size-cover" style="background-position:56.00000000000001% 89%;background-image:url(https://c9.covertnine.com/wp-content/uploads/2020/10/homo-riot-vertical.jpg);background-repeat:no-repeat;background-size:cover"></div></div>
 <!-- /wp:c9-blocks/grid -->`
 };
