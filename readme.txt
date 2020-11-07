3dd48c4672dbd2c464297be51b6e9dd34c28dbda
tim <tim@covertnine.com>
2020-11-06 22:26:32 -0600

    updated link added fontawesome to be local

--- readme.txt
+++ readme.txt
@@ -25,7 +25,7 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 The landing page templates are organized in a modal by the "content journey" step (Awareness, Consideration, Conversion) and can be accessed from the feather logo icon in the upper  right corner after installing the WordPress plugin.
 
-Plugin can be installed from the WordPress admin via file upload, or by cloning the Github repo. We recommend using a [C9 WordPress Theme](https://www.covertnien.com/forms/c9-beta) with the C9 Blocks plugin, but it also works with the Twenty Twenty and Twenty Nineteen WordPress themes.
+Plugin can be installed from the WordPress admin via file upload, or by cloning the Github repo. We recommend using a [C9 WordPress Theme](https://www.covertnine.com/forms/c9-beta) with the C9 Blocks plugin, but it also works with the Twenty Twenty and Twenty Nineteen WordPress themes.
 
 For an overview of each of the blocks, and more up to date documentation, head over to our [documentation  site](https://c9.covertnine.com).
 
@@ -205,7 +205,7 @@ If you're working on behalf of a client or customer and want to build your own s
 
 Please visit our support forum: [Here](https://www.covertnine.com/community/support/)
 
-Get premium paid support: [Here](https://www.covertnien.com/get-support)
+Get premium paid support: [Here](https://www.covertnine.com/get-support)
 
 Main documentation can be found [Here](https://c9.covertnine.com/documentation/)
 
--- src/init.php
+++ src/init.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Blocks Initializer
  *
@@ -9,19 +10,19 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 // Add custom block category.
 add_filter(
 	'block_categories',
-	function ( $categories, $post ) {
+	function ($categories, $post) {
 		return array_merge(
 			array(
 				array(
 					'slug'  => 'c9-blocks',
-					'title' => __( 'COVERT NINE Blocks', 'c9-blocks' ),
+					'title' => __('COVERT NINE Blocks', 'c9-blocks'),
 					'icon'  => '',
 				),
 			),
@@ -35,14 +36,15 @@ add_filter(
 /**
  * Set C9 blocks settings.
  */
-function c9_load_settings() {
+function c9_load_settings()
+{
 	// Register setting.
 	register_setting(
 		'c9_blocks_colors',
 		'c9_blocks_colors',
 		array(
 			'type'              => 'string',
-			'description'       => __( 'Config C9 Blocks Color Palette', 'c9-blocks' ),
+			'description'       => __('Config C9 Blocks Color Palette', 'c9-blocks'),
 			'sanitize_callback' => 'sanitize_text_field',
 			'show_in_rest'      => true,
 			'default'           => '',
@@ -58,9 +60,9 @@ function c9_load_settings() {
 			'items'             => array(
 				'type' => 'string',
 			),
-			'description'       => __( 'Config Theme Color Palette', 'c9-blocks' ),
-			'sanitize_callback' => function( $colors ) {
-				return array_map( 'esc_attr', $colors );
+			'description'       => __('Config Theme Color Palette', 'c9-blocks'),
+			'sanitize_callback' => function ($colors) {
+				return array_map('esc_attr', $colors);
 			},
 			'show_in_rest'      => false,
 			'default'           => array(),
@@ -68,8 +70,8 @@ function c9_load_settings() {
 	);
 }
 
-add_action( 'admin-init', 'c9_load_settings' );
-add_action( 'rest_api_init', 'c9_load_settings' );
+add_action('admin-init', 'c9_load_settings');
+add_action('rest_api_init', 'c9_load_settings');
 
 
 /**
@@ -77,66 +79,69 @@ add_action( 'rest_api_init', 'c9_load_settings' );
  *
  * @return void
  */
-function c9_register_tuts_endpoint() {
+function c9_register_tuts_endpoint()
+{
 	register_rest_route(
 		'c9-blocks/v1',
 		'/tuts',
 		array(
 			'methods'  => 'GET',
 			'callback' => 'c9_get_tuts',
-			'permission_callback'	=> function() {
+			'permission_callback'	=> function () {
 				return true;
 			},
 		)
 	);
 }
-add_action( 'rest_api_init', 'c9_register_tuts_endpoint' );
+add_action('rest_api_init', 'c9_register_tuts_endpoint');
 
 /**
  * Get Covertnine tutorials on hitting rest api endpoint
  *
  * @return string
  */
-function c9_get_tuts() {
+function c9_get_tuts()
+{
 	// https://c9.covertnine.com/wp-json/wp/v2/posts?per_page=50&categories=1 to limit by tutorial categories
 	$response = wp_remote_get(
 		'https://covertnine.com/wp-json/wp/v2/posts?per_page=50&categories[]=727'
 	);
-	$tuts     = json_decode( wp_remote_retrieve_body( $response ) );
+	$tuts     = json_decode(wp_remote_retrieve_body($response));
 	return $tuts;
 }
 
 /**
  * Load Gutenberg Palette
  */
-function c9_load_color_palette() {
-	$theme_palette = get_theme_support( 'editor-color-palette' );
-	update_option( 'c9_orig_colors', $theme_palette );
+function c9_load_color_palette()
+{
+	$theme_palette = get_theme_support('editor-color-palette');
+	update_option('c9_orig_colors', $theme_palette);
 
-	$palette = json_decode( get_option( 'c9_blocks_colors' ) );
-	if ( $palette && is_object( $palette ) && isset( $palette->palette ) && is_array( $palette->palette ) ) {
+	$palette = json_decode(get_option('c9_blocks_colors'));
+	if ($palette && is_object($palette) && isset($palette->palette) && is_array($palette->palette)) {
 		$san_palette = array();
-		foreach ( $palette->palette as $item ) {
+		foreach ($palette->palette as $item) {
 			$san_palette[] = array(
 				'color' => $item->color,
 				'name'  => $item->name,
 				'slug'  => $item->slug,
 			);
 		}
-		if ( isset( $san_palette[0] ) ) {
-			if ( ( isset( $palette->override ) && true !== $palette->override ) || ! isset( $palette->override ) ) {
-				if ( is_array( $theme_palette ) ) {
-					$newpalette = array_merge( reset( $theme_palette ), $san_palette );
+		if (isset($san_palette[0])) {
+			if ((isset($palette->override) && true !== $palette->override) || !isset($palette->override)) {
+				if (is_array($theme_palette)) {
+					$newpalette = array_merge(reset($theme_palette), $san_palette);
 				} else {
 					$default_palette = array();
-					$newpalette      = array_merge( $default_palette, $san_palette );
+					$newpalette      = array_merge($default_palette, $san_palette);
 				}
 			} else {
 				$newpalette = $san_palette;
 			}
-			add_theme_support( 'editor-color-palette', $newpalette );
-			add_action( 'wp_head', 'c9_print_gutenberg_style', 8 );
-			add_action( 'admin_print_styles', 'c9_print_gutenberg_style', 21 );
+			add_theme_support('editor-color-palette', $newpalette);
+			add_action('wp_head', 'c9_print_gutenberg_style', 8);
+			add_action('admin_print_styles', 'c9_print_gutenberg_style', 21);
 		}
 	}
 }
@@ -144,61 +149,63 @@ function c9_load_color_palette() {
 /**
  * Print Gutenberg Palette Styles
  */
-function c9_print_gutenberg_style() {
-	if ( is_admin() ) {
+function c9_print_gutenberg_style()
+{
+	if (is_admin()) {
 		$screen = get_current_screen();
-		if ( ! $screen || ! $screen->is_block_editor() ) {
+		if (!$screen || !$screen->is_block_editor()) {
 			return;
 		}
 	}
-	$palette = json_decode( get_option( 'c9_blocks_colors' ) );
-	if ( $palette && is_object( $palette ) && isset( $palette->palette ) && is_array( $palette->palette ) ) {
+	$palette = json_decode(get_option('c9_blocks_colors'));
+	if ($palette && is_object($palette) && isset($palette->palette) && is_array($palette->palette)) {
 		$san_palette = array();
-		foreach ( $palette->palette as $item ) {
+		foreach ($palette->palette as $item) {
 			$san_palette[] = array(
 				'color' => $item->color,
 				'name'  => $item->name,
 				'slug'  => $item->slug,
 			);
 		}
-		if ( isset( $san_palette[0] ) ) {
+		if (isset($san_palette[0])) {
 			echo '<style id="c9_blocks_palette_css" type="text/css">';
-			foreach ( $san_palette as $set ) {
+			foreach ($san_palette as $set) {
 				$slug  = $set['slug'];
 				$color = $set['color'];
-				echo '.has-' . esc_attr( $slug ) . '-color{color:' . esc_attr( $color ) . '}.has-' . esc_attr( $slug ) . '-background-color{background-color:' . esc_attr( $color ) . '}';
+				echo '.has-' . esc_attr($slug) . '-color{color:' . esc_attr($color) . '}.has-' . esc_attr($slug) . '-background-color{background-color:' . esc_attr($color) . '}';
 			}
 			echo '</style>';
 		}
 	}
 }
-add_action( 'after_setup_theme', 'c9_load_color_palette', 999 );
+add_action('after_setup_theme', 'c9_load_color_palette', 999);
 
 /**
  * Initialize the blocks
  */
-function c9_blocks_loader() {
+function c9_blocks_loader()
+{
 	/**
 	 * Load Social Block PHP
 	 */
-	require_once plugin_dir_path( __FILE__ ) . 'blocks/block-sharing/index.php';
+	require_once plugin_dir_path(__FILE__) . 'blocks/block-sharing/index.php';
 
 	/**
 	 * Load Post Grid PHP
 	 */
-	require_once plugin_dir_path( __FILE__ ) . 'blocks/block-post-grid/index.php';
+	require_once plugin_dir_path(__FILE__) . 'blocks/block-post-grid/index.php';
 
 	/**
 	 * Load Horizontal Tab Component PHP
 	 */
-	require_once plugin_dir_path( __FILE__ ) . 'blocks/block-horizontal-tabs/components/horizontal-tab/index.php';
+	require_once plugin_dir_path(__FILE__) . 'blocks/block-horizontal-tabs/components/horizontal-tab/index.php';
 
 	/**
 	 * Load Vertical Tab Component PHP
 	 */
-	require_once plugin_dir_path( __FILE__ ) . 'blocks/block-vertical-tabs/components/vertical-tab/index.php';
+	require_once plugin_dir_path(__FILE__) . 'blocks/block-vertical-tabs/components/vertical-tab/index.php';
 }
-add_action( 'plugins_loaded', 'c9_blocks_loader' );
+add_action('plugins_loaded', 'c9_blocks_loader');
 
 
 /**
@@ -208,19 +215,19 @@ add_action( 'plugins_loaded', 'c9_blocks_loader' );
  *
  * @since 1.0.0
  */
-function c9_blocks_cgb_block_assets() {
+function c9_blocks_cgb_block_assets()
+{
 	// Styles.
 	wp_enqueue_style(
 		'c9-blocks-style', // Handle.
-		plugins_url( '/dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
+		plugins_url('/dist/blocks.style.build.css', dirname(__FILE__)), // Block style CSS.
 		array(), // Dependency to include the CSS after it.
-		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: filemtime — Gets file modification time.
+		filemtime(plugin_dir_path(__DIR__) . 'dist/blocks.style.build.css') // Version: filemtime — Gets file modification time.
 	);
-
 } // End function c9_blocks_cgb_block_assets().
 
 // Hook: Frontend assets.
-add_action( 'enqueue_block_assets', 'c9_blocks_cgb_block_assets', 120 );
+add_action('enqueue_block_assets', 'c9_blocks_cgb_block_assets', 120);
 
 /**
  * Enqueue Gutenberg block assets for backend editor.
@@ -231,7 +238,8 @@ add_action( 'enqueue_block_assets', 'c9_blocks_cgb_block_assets', 120 );
  *
  * @since 1.0.0
  */
-function c9_blocks_cgb_editor_assets() {
+function c9_blocks_cgb_editor_assets()
+{
 	// Youtube Player API.
 	wp_enqueue_script(
 		'youtube-api',
@@ -242,16 +250,16 @@ function c9_blocks_cgb_editor_assets() {
 	// update category, e.g. add icon and dequeue core blocks we don't want users using.
 	wp_enqueue_script(
 		'c9_blocks-update-category',
-		plugins_url( 'dist/blocks.update-category.build.js', dirname( __FILE__ ) ),
-		array( 'wp-hooks', 'wp-blocks', 'wp-components', 'wp-plugins', 'wp-edit-post', 'wp-element', 'wp-api' )
+		plugins_url('dist/blocks.update-category.build.js', dirname(__FILE__)),
+		array('wp-hooks', 'wp-blocks', 'wp-components', 'wp-plugins', 'wp-edit-post', 'wp-element', 'wp-api')
 	);
 
 	// Scripts.
 	wp_enqueue_script(
 		'c9_blocks-cgb-block-js', // Handle.
-		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-api', 'youtube-api' ), // Dependencies, defined above.
-		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
+		plugins_url('/dist/blocks.build.js', dirname(__FILE__)), // Block.build.js: We register the block here. Built with Webpack.
+		array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-api', 'youtube-api'), // Dependencies, defined above.
+		filemtime(plugin_dir_path(__DIR__) . 'dist/blocks.build.js'), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
 
@@ -260,8 +268,8 @@ function c9_blocks_cgb_editor_assets() {
 		'c9_blocks-update-category',
 		'c9_blocks_params',
 		array(
-			'colors'      => get_option( 'c9_blocks_colors' ),
-			'orig_colors' => get_option( 'c9_orig_colors' ),
+			'colors'      => get_option('c9_blocks_colors'),
+			'orig_colors' => get_option('c9_orig_colors'),
 		)
 	);
 
@@ -270,73 +278,78 @@ function c9_blocks_cgb_editor_assets() {
 		'c9_blocks-cgb-block-js',
 		'c9_blocks_params',
 		array(
-			'colors'      => get_option( 'c9_blocks_colors' ),
-			'orig_colors' => get_option( 'c9_orig_colors' ),
+			'colors'      => get_option('c9_blocks_colors'),
+			'orig_colors' => get_option('c9_orig_colors'),
 		)
 	);
 
 	wp_enqueue_style(
 		'c9_blocks-cgb-block-editor-css', // Handle.
-		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
-		array( 'wp-edit-blocks' ) // Dependency to include the CSS after it.
+		plugins_url('dist/blocks.editor.build.css', dirname(__FILE__)), // Block editor CSS.
+		array('wp-edit-blocks') // Dependency to include the CSS after it.
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: filemtime — Gets file modification time.
 	);
 } // End function c9_blocks_cgb_editor_assets().
 
 // Hook: Editor assets.
-add_action( 'enqueue_block_editor_assets', 'c9_blocks_cgb_editor_assets', 9999 );
+add_action('enqueue_block_editor_assets', 'c9_blocks_cgb_editor_assets', 9999);
 
 /**
  * Utility function, check for bootstrap for common handle names, if no match, enqueue bootstrap
  */
-function c9_check_bootstrap() {
-	$bootstrap_handles = array( 'bootstrap', 'bootstrap-js', 'bootstrap-css', 'bootstrapcss', 'bootstrap4', 'bootstrap4css', 'bootstrap4-css', 'c9-styles' );
+function c9_check_bootstrap()
+{
+	$bootstrap_handles = array('bootstrap', 'bootstrap-js', 'bootstrap-css', 'bootstrapcss', 'bootstrap4', 'bootstrap4css', 'bootstrap4-css', 'c9-styles');
 
 	/**
 	 * Utility function, check for bootstrap for common handle names, if no match, enqueue bootstrap
 	 *
 	 * @param string $handle The handle to check.
 	 */
-	function check_handle_exist( $handle ) {
-		return wp_style_is( $handle, 'queue' ) || wp_style_is( $handle, 'done' );
+	if (!function_exists('c9_check_handle_exist')) {
+		function c9_check_handle_exist($handle)
+		{
+			return wp_style_is($handle, 'queue') || wp_style_is($handle, 'done');
+		}
 	}
 
 	$checks = array_filter(
-		array_map( 'check_handle_exist', $bootstrap_handles ),
-		function ( $c ) {
+		array_map('c9_check_handle_exist', $bootstrap_handles),
+		function ($c) {
 			return $c;
 		}
 	);
 
 	// if any of them matches, then array length of $check > 0.
-	if ( sizeof( $checks ) === 0 ) {
+	if (sizeof($checks) === 0) {
 		// Styles.
-		wp_enqueue_style( 'c9-fontawesome', '//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array( 'c9-blocks-style' ), '4.7.1' );
+		wp_enqueue_style('c9-fontawesome', plugins_url('src/vendor/font-awesome.min.css'), array('c9-blocks-style'), '4.7.1');
 		wp_enqueue_style(
 			'c9-bootstrap',
-			plugins_url( 'dist/blocks.bootstrap.build.css', dirname( __FILE__ ) ),
-			array( 'c9-blocks-style' ),
+			plugins_url('dist/blocks.bootstrap.build.css', dirname(__FILE__)),
+			array('c9-blocks-style'),
 			'4.3.1'
 		);
 
 		wp_enqueue_script(
 			'c9-bootstrap-js',
-			plugins_url( 'src/vendor/c9-bootstrap.min.js', dirname( __FILE__ ) ),
-			array( 'jquery' ),
+			plugins_url('src/vendor/c9-bootstrap.min.js', dirname(__FILE__)),
+			array('jquery'),
 			'4.3.1'
 		);
 	}
 }
 
 // Check bootstrap - frontend.
-add_action( 'enqueue_block_editor_assets', 'c9_check_bootstrap', 130 );
-add_action( 'wp_enqueue_scripts', 'c9_check_bootstrap', 100 );
+add_action('enqueue_block_editor_assets', 'c9_check_bootstrap', 130);
+add_action('wp_enqueue_scripts', 'c9_check_bootstrap', 100);
 
 
 /**
  * Initialize the blocks frontend
  */
-function c9_blocks_front_assets() {
+function c9_blocks_front_assets()
+{
 	// Youtube Player API.
 	wp_enqueue_script(
 		'youtube-api',
@@ -347,59 +360,63 @@ function c9_blocks_front_assets() {
 	// blocks frontend.
 	wp_enqueue_script(
 		'c9_blocks-frontend',
-		plugins_url( 'dist/blocks.frontend.build.js', dirname( __FILE__ ) ),
+		plugins_url('dist/blocks.frontend.build.js', dirname(__FILE__)),
 		array('jquery')
 	);
 }
 
 // Hook: c9 Blocks Frontend.
-add_action( 'wp_enqueue_scripts', 'c9_blocks_front_assets', 110 );
+add_action('wp_enqueue_scripts', 'c9_blocks_front_assets', 110);
 
 /**
  * Create Link to Manage Reusable Blocks in Appearance Menu
  *
  * @return void
  */
-function c9_add_reusable_link() {
+function c9_add_reusable_link()
+{
 	global $submenu;
 	$permalink               = admin_url() . 'edit.php?post_type=wp_block';
-	$submenu['themes.php'][] = array( 'C9 Reusable Blocks', 'manage_options', $permalink );
+	$submenu['themes.php'][] = array('C9 Reusable Blocks', 'manage_options', $permalink);
 }
-add_action( 'admin_menu', 'c9_add_reusable_link' );
+add_action('admin_menu', 'c9_add_reusable_link');
 
 /**
  * Add Getting Started Page to Appearance Menu
  *
  * @return void
  */
-function c9_add_getting_started_page() {
-	add_theme_page( 'C9 Blocks', 'C9 Blocks', 'manage_options', 'c9-blocks-getting-started', 'c9_render_getting_started' );
+function c9_add_getting_started_page()
+{
+	add_theme_page('C9 Blocks', 'C9 Blocks', 'manage_options', 'c9-blocks-getting-started', 'c9_render_getting_started');
 }
-add_action( 'admin_menu', 'c9_add_getting_started_page' );
+add_action('admin_menu', 'c9_add_getting_started_page');
 
 /**
  * Render Admin Page
  *
  * @return void
  */
-function c9_render_getting_started() {
-	include_once( plugin_dir_path( __DIR__ ) . 'admin/class-getting-started.php' );
+function c9_render_getting_started()
+{
+	include_once(plugin_dir_path(__DIR__) . 'admin/class-getting-started.php');
 	$tuts = c9_get_tuts();
-	$page = C9_Block_Getting_Started::render( $tuts );
+	$page = C9_Block_Getting_Started::render($tuts);
 	return $page;
 }
 
 /**
  * Conditionally enqueue admin assets
  */
-function c9_enqueue_admin_style( $hook_suffix ) {
-	if ( 'appearance_page_c9-blocks-getting-started' !== $hook_suffix ) {
+function c9_enqueue_admin_style($hook_suffix)
+{
+	if ('appearance_page_c9-blocks-getting-started' !== $hook_suffix) {
 		return;
 	}
 
 	// Load your css.
-	wp_register_style( 'c9_admin_css', plugin_dir_url( __DIR__ ) . 'admin/admin-style.css', false, '1.0.0' );
-	wp_enqueue_style( 'c9_admin_css' );
+	wp_register_style('c9_admin_css', plugin_dir_url(__DIR__) . 'admin/admin-style.css', false, '1.0.0');
+	wp_enqueue_style('c9_admin_css');
 }
 
-add_action( 'admin_enqueue_scripts', 'c9_enqueue_admin_style' );
+add_action('admin_enqueue_scripts', 'c9_enqueue_admin_style');
