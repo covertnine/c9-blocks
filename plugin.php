0fbf4adeeb51e20105b8c2b9cb8fecd041355929
tim <tim@covertnine.com>
2020-11-07 18:24:02 -0600

    optimizing icons and adding updates to readmes

--- plugin.php
+++ plugin.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Plugin Name: C9 Blocks
  * Plugin URI: https://www.c9blocks.com
  * Description: 10 new Gutenberg blocks with section templates and page templates that use Bootstrap + CSS Grid to make responsive page building with the native Blocks API a breeze. Compatible with the C9 Starter, C9 Work, C9 Togo, Twenty Nineteen, and Twenty Twenty WordPress themes.
  * Author: COVERT NINE
  * Author URI: https://www.covertnine.com
- * Version: 1.2
+ * Version: 1.2.1
  * License: GPL2+
  * Text Domain: c9-blocks
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -14,37 +15,39 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 /**
  * Block Initializer.
  */
-require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
+require_once plugin_dir_path(__FILE__) . 'src/init.php';
 
 /**
  * Adds a redirect option during plugin activation on non-multisite installs.
  *
  * @param bool $network_wide Whether or not the plugin is being network activated.
  */
-function c9_blocks_activate( $network_wide = false ) {
+function c9_blocks_activate($network_wide = false)
+{
 	// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Only used to do a redirect. False positive.
-	if ( ! $network_wide && ! isset( $_GET['activate-multi'] ) ) {
-		add_option( 'c9_blocks_do_activation_redirect', true );
+	if (!$network_wide && !isset($_GET['activate-multi'])) {
+		add_option('c9_blocks_do_activation_redirect', true);
 	}
 }
-register_activation_hook( __FILE__, 'c9_blocks_activate' );
+register_activation_hook(__FILE__, 'c9_blocks_activate');
 
 
 /**
  * Redirect to the C9 Blocks Getting Started page on single plugin activation.
  */
-function c9_blocks_redirect() {
-	if ( get_option( 'c9_blocks_do_activation_redirect', false ) ) {
-		delete_option( 'c9_blocks_do_activation_redirect' );
-		wp_safe_redirect( esc_url( admin_url( 'themes.php?page=c9-blocks-getting-started' ) ) );
+function c9_blocks_redirect()
+{
+	if (get_option('c9_blocks_do_activation_redirect', false)) {
+		delete_option('c9_blocks_do_activation_redirect');
+		wp_safe_redirect(esc_url(admin_url('themes.php?page=c9-blocks-getting-started')));
 		exit;
 	}
 }
-add_action( 'admin_init', 'c9_blocks_redirect' );
+add_action('admin_init', 'c9_blocks_redirect');
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://www.covertnine.com/about
 Tags: blocks, gutenberg, page builder, block, marketing, block editor, gutenberg blocks, editor, landing page, responsive, blocks
 Requires at least: 5.3
 Tested up to: 5.5
-Stable tag: 1.2
+Stable tag: 1.2.1
 Requires PHP: 7.1
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -60,6 +60,10 @@ Yes, you will need to have WordPress 5.0 or later installed to take advantage of
 6. Tutorials in the modal to show how to use the blocks plugin and how to build landing pages.
 
 == Changelog ==
+
+= 1.2.1 =
+
+Optimized SVG Icons.
 
 = 1.2 =
 Added alignwide and alignfull alignments to Carousels and Image Carousels. Added draggable heights for containers in C9 Grids. Several bug fixes to jumpy navigations, preparing plugins for animation settings. Bug fixes to alignments on some blocks. New page templates and section templates!
