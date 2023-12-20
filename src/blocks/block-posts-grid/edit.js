a830b3326b26ae0c9ab5029016e208d0cbd25931
COVERT NINE <covertnine@users.noreply.github.com>
2023-12-20 16:53:58 -0600

    fixed warning from posts grid, and bug with floated images in grids

--- src/blocks/block-posts-grid/edit.js
+++ src/blocks/block-posts-grid/edit.js
@@ -1,18 +1,9 @@
-/**
- * Internal dependencies
- */
 import Inspector from './components/inspector';
 import Container from './components/container';
 
-/**
- * WordPress dependencies
- */
 const { Component, Fragment } = wp.element;
 const { InnerBlocks, BlockControls } = wp.blockEditor;
 
-/**
- * External Dependencies.
- */
 import cryptoRandomString from 'crypto-random-string';
 
 const ALLOWED_BLOCKS = ['c9-blocks/post-grid'];
@@ -22,41 +13,46 @@ class Edit extends Component {
 		super(...arguments);
 	}
 
-	componentDidUpdate() {
+	componentDidMount() {
+		this.initializeInstanceId();
+	}
+
+	componentDidUpdate(prevProps) {
 		this.checkBlockIdAndUpdate();
+		this.initializeInstanceId(prevProps);
+	}
+
+	initializeInstanceId(prevProps = {}) {
+		const { instanceId } = this.props.attributes;
+		if (
+			instanceId === undefined &&
+			instanceId !== prevProps.attributes.instanceId
+		) {
+			const newInstanceId =
+				this.props.instanceId ||
+				parseInt(cryptoRandomString({ length: 4, type: 'numeric' }));
+			this.props.setAttributes({ instanceId: newInstanceId });
+		}
 	}
 
 	checkBlockIdAndUpdate = () => {
 		const { attributes, setAttributes } = this.props;
-
 		const { instanceId, containerVideoID } = attributes;
 
-		// check for possible id collision
 		if (
 			instanceId !== undefined &&
-			1 <
-				document.querySelectorAll(`#player-${containerVideoID}-${instanceId}`)
-					.length
+			document.querySelectorAll(`#player-${containerVideoID}-${instanceId}`)
+				.length > 1
 		) {
 			const newInstanceId = parseInt(
 				cryptoRandomString({ length: 4, type: 'numeric' })
 			);
-
-			setAttributes({
-				instanceId: newInstanceId,
-			});
+			setAttributes({ instanceId: newInstanceId });
 		}
 	};
 
 	render() {
-		const { attributes, setAttributes } = this.props;
-		let instanceId = attributes.instanceId;
-
-		if (instanceId === undefined) {
-			// set default random id if not set
-			instanceId = this.props.instanceId;
-			setAttributes({ instanceId });
-		}
+		//const { attributes } = this.props; no longer needed
 
 		return (
 			<Fragment>
